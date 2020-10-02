52f5acc9ce56837ffcf7c6c50bd4aee42f118d08
Arban Dyrmishi <arban.dyrmishi@beantech.it>
2020-10-02 10:51:15 +0200

    End of section 3

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'The dating app';
+  users: any;
+  baseUrl: string = 'https://localhost:5001/api/'
+  constructor(private http : HttpClient){}
+
+  ngOnInit() {
+    this.getUsers();
+  }
+
+  getUsers(){
+    this.http.get(this.baseUrl + 'users').subscribe( response => {
+      this.users = response;
+    }, error => {
+      console.log(error)
+    })
+  }
+
+
+}
